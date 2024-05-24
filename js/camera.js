1e3256416ba342fc52d32fe18f5d48d409ad32d6
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-05-24 18:47:21 -0300

    capturar foto pela camera

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,11 @@
 const botaoIniciarCamera = document.querySelector('[data-video-botao]');
 const campoCamera = document.querySelector('[data-camera]');
 const video = document.querySelector('[data-video]');
+const botaoTirarFoto = document.querySelector('[data-tirar-foto]');
+const canvas = document.querySelector('[data-video-canvas]');
+const mensagem = document.querySelector('[data-mensagem]');
+
+let imagemURL = '';
 
 botaoIniciarCamera.addEventListener('click', async function () {
   const iniciarVideo = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
@@ -9,4 +14,13 @@ botaoIniciarCamera.addEventListener('click', async function () {
   campoCamera.style.display = 'block';
 
   video.srcObject = iniciarVideo;
+})
+
+botaoTirarFoto.addEventListener('click', function () {
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  imagemURL = canvas.toDataURL('image/jpeg'); // transforma a imagem em uma URL
+
+  campoCamera.style.display = 'none';
+  mensagem.style.display = 'block';
 })
